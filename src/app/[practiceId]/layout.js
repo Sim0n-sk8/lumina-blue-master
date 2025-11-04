84e2c6169a7cec41267598dc3f9425adbf67a4ef
Simon Swart <simonswart91@gmail.com>
2025-11-04 14:17:07 +0200

    spsjdlkjfwdsbaslkgbalkjfelkjabdsflkvjbasdlf

--- src/app/[practiceId]/layout.js
+++ src/app/[practiceId]/layout.js
@@ -4,12 +4,12 @@ export default async function CustomerLayout({ children, params }) {
   // Fetch all settings server-side
   const siteSettings = await getSiteSettings(params.practiceId);
 
-  const siteName = siteSettings?.name || "Lumina Blue";
+  const siteName = siteSettings?.practiceName || "Lumina Blue";
   const description =
     siteSettings?.aboutText || `${siteName} - Eye care services in ${siteSettings?.city || "your area"}`;
   const ogImageRaw =
-  siteSettings?.banners?.length > 0
-    ? siteSettings.banners[0].img
+  siteSettings?.bannerImage?.length > 0
+    ? siteSettings.bannerImage[0].img
     : "/default-banner.jpg";
 
   const ogImage = ogImageRaw
